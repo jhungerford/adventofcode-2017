6d80cb624ab7486506a3aaa4866b3f805893426a
John Hungerford <john.hungerford@cloudability.com>
2017-12-22 11:14:22 -0700

    Day 7 part 1.

---
+++ day7/day7.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"regexp"
+	"strconv"
+	"strings"
+	"fmt"
+	"os"
+	"bufio"
+	"container/list"
+)
+
+type ParsedRow struct {
+	name          string
+	weight        int
+	childrenNames []string
+}
+
+// Parses a row like 'pbga (66)' or 'fwft (72) -> ktlj, cntj, xhth'
+func parseRow(row string) ParsedRow {
+	r := regexp.MustCompile("^([a-z]+) \\(([0-9]+)\\)(?: -> ([a-z, ]+))?$")
+
+	groups := r.FindStringSubmatch(row)
+
+	if groups == nil {
+		panic("Invalid row '" + row + "'")
+	}
+
+	weight, _ := strconv.Atoi(groups[2])
+	var childrenNames []string
+	if groups[3] != "" {
+		childrenNames = strings.Split(groups[3], ", ")
+	}
+
+	return ParsedRow{groups[1], weight, childrenNames}
+}
+
+type Node struct {
+	name     string
+	weight   int
+	parent   *Node
+	children []*Node
+}
+
+// Given an array of parsed nodes, builds and returns a tree of nodes
+func buildTree(parsedRows []ParsedRow) *Node {
+	m := make(map[string]*Node)
+
+	for _, parsedRow := range parsedRows {
+		m[parsedRow.name] = &Node{parsedRow.name, parsedRow.weight, nil, []*Node{}}
+	}
+
+	var root *Node
+	for _, parsedRow := range parsedRows {
+		node := m[parsedRow.name]
+
+		node.children = make([]*Node, len(parsedRow.childrenNames))
+		for i, name := range parsedRow.childrenNames {
+			child := m[name]
+			node.children[i] = child
+			child.parent = node
+		}
+
+		root = node
+	}
+
+	for root.parent != nil {
+		root = root.parent
+	}
+
+	return root
+}
+
+// Reads newline separated lines from the file with the given name
+func readLines(fileName string) ([]string, error) {
+	f, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+
+	defer f.Close()
+
+	s := bufio.NewScanner(f)
+	l := list.New()
+
+	for s.Scan() {
+		row := strings.TrimSpace(s.Text())
+		if row != "" {
+			l.PushBack(row)
+		} else {
+			fmt.Print("Blank row")
+		}
+	}
+
+	lines := make([]string, l.Len())
+
+	i := 0
+	for e := l.Front(); e != nil; e = e.Next() {
+		lines[i] = e.Value.(string)
+		i ++
+	}
+
+	return lines, nil
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Print("Usage: ", os.Args[0], " <input file>")
+		return
+	}
+
+	lines, err := readLines(os.Args[1])
+	if err != nil {
+		fmt.Print("Error reading", os.Args[1], err)
+		return
+	}
+
+	parsedRows := make([]ParsedRow, len(lines))
+	for i, row := range lines {
+		parsedRows[i] = parseRow(row)
+	}
+
+	tree := buildTree(parsedRows)
+
+	fmt.Print("Part 1: ", tree.name)
+}
---
+++ day7/day7_test.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"testing"
+)
+
+type ParseRowTestPair struct {
+	input string
+	expected ParsedRow
+}
+
+func TestParseRow(t *testing.T) {
+	tests := []ParseRowTestPair{
+		{"pbga (66)", ParsedRow{"pbga", 66, []string{}}},
+		{"xhth (57)", ParsedRow{"xhth", 57, []string{}}},
+		{"ebii (61)", ParsedRow{"ebii", 61, []string{}}},
+		{"havc (66)", ParsedRow{"havc", 66, []string{}}},
+		{"ktlj (57)", ParsedRow{"ktlj", 57, []string{}}},
+		{"fwft (72) -> ktlj, cntj, xhth", ParsedRow{"fwft", 72, []string{"ktlj", "cntj", "xhth"}}},
+		{"qoyq (66)", ParsedRow{"qoyq", 66, []string{}}},
+		{"padx (45) -> pbga, havc, qoyq", ParsedRow{"padx", 45, []string{"pbga", "havc", "qoyq"}}},
+		{"tknk (41) -> ugml, padx, fwft", ParsedRow{"tknk", 41, []string{"ugml", "padx", "fwft"}}},
+		{"jptl (61)", ParsedRow{"jptl", 61, []string{}}},
+		{"ugml (68) -> gyxo, ebii, jptl", ParsedRow{"ugml", 68, []string{"gyxo", "ebii", "jptl"}}},
+		{"gyxo (61)", ParsedRow{"gyxo", 61, []string{}}},
+		{"cntj (57)", ParsedRow{"cntj", 57, []string{}}},
+	}
+
+	for _, test := range tests {
+		actual := parseRow(test.input)
+
+		if !parsedRowsEqual(test.expected, actual) {
+			t.Error("Wrong result for parseRow", test.input, " - expected:", test.expected, "actual:", actual)
+		}
+	}
+}
+
+func parsedRowsEqual(a ParsedRow, b ParsedRow) bool {
+	if a.name != b.name || a.weight != b.weight {
+		return false
+	}
+
+	if len(a.childrenNames) != len(b.childrenNames) {
+		return false
+	}
+
+	for i, av := range a.childrenNames {
+		if av != b.childrenNames[i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func TestBuildTree(t *testing.T) {
+	expected := Node{"tknk", 41, nil, []*Node{
+		{"ugml", 68, nil, []*Node{
+			{"gyxo", 61, nil, []*Node{}},
+			{"ebii", 61, nil, []*Node{}},
+			{"jptl", 61, nil, []*Node{}},
+		}},
+		{"padx", 45, nil, []*Node{
+			{"pbga", 66, nil, []*Node{}},
+			{"havc", 66, nil, []*Node{}},
+			{"qoyq", 66, nil, []*Node{}},
+			}},
+		{"fwft", 72, nil, []*Node{
+			{"ktlj", 57, nil, []*Node{}},
+			{"cntj", 57, nil, []*Node{}},
+			{"xhth", 57, nil, []*Node{}},
+			}},
+	}}
+
+	parsedRows := []ParsedRow {
+		{"pbga", 66, []string{}},
+		{"xhth", 57, []string{}},
+		{"ebii", 61, []string{}},
+		{"havc", 66, []string{}},
+		{"ktlj", 57, []string{}},
+		{"fwft", 72, []string{"ktlj", "cntj", "xhth"}},
+		{"qoyq", 66, []string{}},
+		{"padx", 45, []string{"pbga", "havc", "qoyq"}},
+		{"tknk", 41, []string{"ugml", "padx", "fwft"}},
+		{"jptl", 61, []string{}},
+		{"ugml", 68, []string{"gyxo", "ebii", "jptl"}},
+		{"gyxo", 61, []string{}},
+		{"cntj", 57, []string{}},
+	}
+
+	actual := buildTree(parsedRows)
+
+	if !treeEqual(&expected, actual) {
+		t.Error("Wrong result for buildTree.  Expected", expected, "Actual", actual)
+	}
+}
+
+func treeEqual(expected *Node, actual *Node) bool {
+	if expected.name != actual.name || expected.weight != actual.weight {
+		return false
+	}
+
+	if len(expected.children) != len(actual.children) {
+		return false
+	}
+
+	for i, above := range expected.children {
+		if !treeEqual(above, actual.children[i]) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func TestReadLines(t *testing.T) {
+	lines, err := readLines("testdata/lines.txt")
+
+	if err != nil {
+		t.Error("Error reading file", err)
+	}
+
+	if len(lines) != 13 {
+		t.Error("Wrong number of lines")
+	}
+
+	if lines[0] != "pbga (66)" {
+		t.Error("Wrong value for line 1")
+	}
+
+	if lines[12] != "cntj (57)" {
+		t.Error("Wrong value for line 13")
+	}
+}
