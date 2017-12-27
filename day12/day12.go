6feee3a573c58d2b6d0addf00863156a789dea59
John Hungerford <john.hungerford@cloudability.com>
2017-12-27 10:09:37 -0700

    Day 12 part 1.

---
+++ day12/day12.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"regexp"
+	"strconv"
+	"strings"
+	"container/list"
+	"os"
+	"fmt"
+	"bufio"
+)
+
+type Graph struct {
+	// Adjacency list.  Each element in the array is the set of neighbor vertexes
+	// https://en.wikipedia.org/wiki/Adjacency_list
+	adjacent [][]int
+}
+
+// Constructs a new graph that has the given number of vertexes
+func NewGraph(size int) *Graph {
+	return &Graph{make([][]int, size)}
+}
+
+// Parses the given row in the format vertex <-> neighbors, modifying the graph to store the neighbors
+var rowRegex = regexp.MustCompile("(\\d+) <-> ([0-9, ]+)")
+func parse(row string, graph *Graph) {
+	if row == "" {
+		return
+	}
+
+	groups := rowRegex.FindStringSubmatch(row)
+
+	if groups == nil {
+		panic("Invalid row '" + row + "'")
+	}
+
+	vertex, err := strconv.Atoi(groups[1])
+	if err != nil {
+		panic(err)
+	}
+
+	neighborStrs := strings.Split(groups[2], ", ")
+	neighbors := make([]int, len(neighborStrs))
+
+	for i, neighborStr := range neighborStrs {
+		neighbors[i], err = strconv.Atoi(neighborStr)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	graph.adjacent[vertex] = neighbors
+}
+
+// Return the size of the spanning tree connected to the given node
+func spanningTreeSize(graph *Graph, node int) int {
+	visited := make(map[int]bool)
+
+	toVisit := list.New()
+
+	toVisit.PushBack(node)
+	visited[node] = true
+
+	for toVisit.Len() > 0 {
+		current := toVisit.Front()
+		toVisit.Remove(current)
+
+		for _, neighbor := range graph.adjacent[current.Value.(int)] {
+			if !visited[neighbor] {
+				toVisit.PushBack(neighbor)
+				visited[neighbor] = true
+			}
+		}
+	}
+
+	return len(visited)
+}
+
+// Reads newline separated lines from the file with the given name
+func readLines(fileName string) ([]string, error) {
+	f, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+
+	defer f.Close()
+
+	s := bufio.NewScanner(f)
+	l := list.New()
+
+	for s.Scan() {
+		row := strings.TrimSpace(s.Text())
+		if row != "" {
+			l.PushBack(row)
+		}
+	}
+
+	lines := make([]string, l.Len())
+
+	i := 0
+	for e := l.Front(); e != nil; e = e.Next() {
+		lines[i] = e.Value.(string)
+		i ++
+	}
+
+	return lines, nil
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Print("Usage: ", os.Args[0], " <input file>")
+		return
+	}
+
+	lines, err := readLines(os.Args[1])
+	if err != nil {
+		fmt.Print("Error reading", os.Args[1], err)
+		return
+	}
+
+	graph := NewGraph(2000)
+
+	for _, line := range lines {
+		parse(line, graph)
+	}
+
+	fmt.Println("Part 1:", spanningTreeSize(graph, 0))
+
+}
---
+++ day12/day12_test.go
@@ -0,0 +1,71 @@
+package main
+
+import "testing"
+
+var exampleGraph = &Graph{
+[][]int{
+{2},
+{1},
+{0, 3, 4},
+{2, 4},
+{2, 3, 6},
+{6},
+{4, 5},
+},
+}
+
+func TestParse(t *testing.T) {
+	rows := []string{
+		"0 <-> 2",
+		"1 <-> 1",
+		"2 <-> 0, 3, 4",
+		"3 <-> 2, 4",
+		"4 <-> 2, 3, 6",
+		"5 <-> 6",
+		"6 <-> 4, 5",
+	}
+
+	graph := NewGraph(7)
+
+	for _, row := range rows {
+		parse(row, graph)
+	}
+
+	if !graphsMatch(graph, exampleGraph) {
+		t.Error("Parse failed.  Expected:", exampleGraph, "Actual:", graph)
+	}
+}
+
+func TestSpanningTreeSize(t *testing.T) {
+	if spanningTreeSize(exampleGraph, 0) != 6 {
+
+	}
+
+	if spanningTreeSize(exampleGraph, 1) != 1 {
+		t.Error("Wrong spanning tree size for node 1")
+	}
+}
+
+func checkSpanningTreeSize(graph *Graph, node int) {
+
+}
+
+func graphsMatch(a, b *Graph) bool {
+	if len(a.adjacent) != len(b.adjacent) {
+		return false
+	}
+
+	for i, neighbors := range a.adjacent {
+		if len(neighbors) != len(b.adjacent[i]) {
+			return false
+		}
+
+		for j, neighbor := range neighbors {
+			if neighbor != b.adjacent[i][j] {
+				return false
+			}
+		}
+	}
+
+	return true
+}
